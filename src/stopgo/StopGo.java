ff107241480ec8fbad61942d239980a43329952e
ST10517326 <ST10517326@myemeris.edu.za>
2026-02-25 14:18:43 +0200

    Final Submission

---
+++ src/stopgo/StopGo.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stopgo;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class StopGo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("             --------------");
+        System.out.println("\t   /\\\t\t    /\\\t");                
+        System.out.println("\t  /  \\\t\t   /  \\\t");
+        System.out.println("\t /STOP\\\t\t  / GO \\\t");
+        System.out.println("\t/_ _ _ \\\t /_ _ _ \\\t");
+        System.out.println("           **               XX");
+        System.out.println("           **               XX");
+        System.out.println("           **               XX");
+        System.out.println("           **               XX");
+        System.out.println("           **               XX");
+        System.out.println("          *****            XXXXX");
+        System.out.println("       ***********      XXXXXXXXXX\n");
+        System.out.println("|-------------------------------------------------|");
+        System.out.println("|-------------------------------------------------|");
+
+
+    }
+      
+}
